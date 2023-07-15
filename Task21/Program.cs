4243afc9168d5df6c7a7bcb9c2a35c68411fde27
Maksim <maksimkon2012945@gmail.com>
2023-07-15 22:48:45 +0300

    Task23 end

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу которая принимает на вход
+// координаты 3 точек и находит растояние между ними в 3D пространстве
+
+
--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу которая , которая принимает на вход число (И) и выдает таблицу кубов чисел
+
+
+int Inpute(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int N = Inpute("число мне ! ");
+int i = 1;
+
+while (i <= N)
+{
+    Console.WriteLine($"хоба ! {i * i * i}");
+    i++;
+}
